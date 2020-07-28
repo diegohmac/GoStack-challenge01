9e89282155fa0686acc81c6cf183105e239668c7
Diego Machado <machadodiegoh@gmail.com>
2020-07-27 23:38:07 -0300

    added logic to list, add, edit, delete and increase like

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid, isUuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,65 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const newProject = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(newProject);
+
+  return response.status(201).json(newProject);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  if (!isUuid(id))
+    return response.status(400).json({ error: "Oops! Invalid ID." });
+
+  const projectIndex = repositories.findIndex((repo) => repo.id === id);
+  const project = repositories.splice(projectIndex, 1)[0];
+  project.title = title;
+  project.url = url;
+  project.techs = techs;
+
+  repositories.push(project);
+  return response.status(200).json(project);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (!isUuid(id))
+    return response
+      .status(400)
+      .json({ error: "Oops! There is no repository with this ID." });
+
+  const projectIndex = repositories.findIndex((repo) => repo.id === id);
+  repositories.splice(projectIndex, 1);
+  return response.status(204).json();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  if (!isUuid(id))
+    return response.status(400).json({ error: "Oops! Invalid ID." });
+
+  const projectIndex = repositories.findIndex((repo) => repo.id === id);
+  repositories[projectIndex].likes += 1;
+
+  return response.status(200).json(repositories[projectIndex]);
 });
 
 module.exports = app;
